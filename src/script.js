83d70451eb70eac096a1100c7dfb0cabce757497
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-10-04 01:02:59 +0600

    feat(form-card): add user input form with dynamic card generation and JS functionality

---
+++ src/script.js
@@ -0,0 +1,57 @@
+let input_value = document.querySelectorAll("input")
+let form = document.querySelector("form")
+
+let part2 = document.getElementById("part2")
+
+
+
+form.addEventListener("submit", (dtls) => {
+    dtls.preventDefault()// stoped reload
+
+    let div = document.createElement("div")
+    div.className = "input_card  bg-[#1A1A1A] text-white w-80 flex flex-col items-center content-center p-4";
+
+
+    let imgDiv = document.createElement("div")
+    imgDiv.className = "h-16 w-15 rounded-full overflow-hidden";
+
+    let img = document.createElement("img")
+    img.setAttribute("src", input_value[1].value)
+
+    let h1 = document.createElement("h1")
+    h1.className = "font-bold";
+    h1.textContent = input_value[0].value;
+
+    let p1 = document.createElement("p")
+    p1.className = "font-semibold"
+    p1.textContent = input_value[2].value
+
+    let p2 = document.createElement("p")
+    p2.className = "text-center"
+    p2.textContent = input_value[3].value;
+
+
+
+
+
+
+    imgDiv.appendChild(img)
+    div.appendChild(imgDiv)
+
+    div.appendChild(h1)
+    div.appendChild(p1)
+    div.append(p2)
+
+    part2.appendChild(div)
+
+
+    input_value.forEach(function (dts) {
+
+        if (dts.value !== "submit") {
+            dts.value = "";
+        }
+
+
+    })
+
+})
